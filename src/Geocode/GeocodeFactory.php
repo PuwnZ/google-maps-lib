ea71c7a7f40fc7988b9fa0da9a1f7e54982dc30a
Guillaume D <g.deloince+dev@gmail.com>
2020-06-30 23:04:27 +0200

    improvement(cache): add cahing system (#27)
    * improvement(cache): add cahing system
    
    * fix(cache): fix cache
    
    * test(units): add units tests

--- src/Geocode/GeocodeFactory.php
+++ src/Geocode/GeocodeFactory.php
@@ -6,17 +6,20 @@ namespace Puwnz\GoogleMapsLib\Geocode;
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpClient\HttpClient;
 
 class GeocodeFactory
 {
-    public static function create(string $googleApiKey, string $logFilePath = '/var/logs/geocode.log', float $httpVersion = 2.0) : GeocodeParser
+    public static function create(string $googleApiKey, string $logFilePath = './var/logs/geocode.log', float $httpVersion = 2.0) : GeocodeParser
     {
         $client = HttpClient::create(['http_version' => $httpVersion]);
         $logger = new Logger('geocode');
         $logger->pushHandler(new StreamHandler($logFilePath, Logger::DEBUG));
 
-        $geocodeClient = new GeocodeClient($client, $logger, $googleApiKey);
+        $cache = new FilesystemAdapter('google-geocode', 0, './var/cache');
+
+        $geocodeClient = new GeocodeClient($client, $logger, $cache, $googleApiKey);
         $geocodeResults = new GeocodeResultsFactory($logger);
 
         return new GeocodeParser($geocodeClient, $geocodeResults);
